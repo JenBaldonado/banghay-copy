d0af377943c46279545623cf7566ad7e92653cf5
Jenza <spotty0326@gmail.com>
2022-10-14 20:18:55 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// /* document.getElementById("#upload").addEventlistener("click"), fuction(){
+//   document.querySelector(".upload-container").style.display = "flex";
+// };
+
+// document.getElementById("#close").addEventlistener("click"), fuction(){
+//   document.querySeletor(".upload-container").style.display = "none";
+// }; */
+
+// var popup = document.getElementById("upload-popup");
+// var btn = document.getElementById("sharebtn");
+// var span = document.getElementsByClassName("closebtn")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   popup.style.display = "block";
+// };
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   popup.style.display = "none";
+// };
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function (event) {
+//   if (event.target == popup) {
+//     popup.style.display = "none";
+//   }
+// };
